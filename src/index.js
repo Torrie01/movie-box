63855449a3490bc418fb489dcd9db604866e8dbd
Torrie01 <t.asifor@yahoo.com>
2023-12-21 23:56:40 +0100

    My commit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import 'react-toastify/dist/ReactToastify.css';
 import App from './App';
 import { AuthProvider } from './Components/AuthContext';
 
