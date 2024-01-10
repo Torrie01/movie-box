cdc85b6d10c54178a7d63fb648df35113777d193
Torrie01 <t.asifor@yahoo.com>
2024-01-10 12:32:35 +0100

    my Final commit

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -7,12 +7,12 @@ const Footer = () => {
     <div className='footer'>
         <div className='footer-icons'>
         <IconContext.Provider value={{ color: "black", size:'1.5rem', className: "global-class-name" }}>
-            <a href='https://www.facebook.com/ToritsemugboneA/'><FaFacebook /></a>
-            <a href='https://www.instagram.com/pricelesstori/'><FaInstagram /></a>
-            <a href='https://www.twitter.com/ToritsemugboneA/'><FaTwitter /></a>
-            <a href='https://www.linkedin.com/in/torrie01/'><FaLinkedin /></a>
-            <a href='https://www.youtube.com/torrie/'><FaYoutube /></a>
-            <a href='https://portfolio-1-m4oojzuki-gbonetoritse-gmailcom.vercel.app/'><FaLink /></a>
+            <a href='https://www.facebook.com/ToritsemugboneA/' target='blank'><FaFacebook /></a>
+            <a href='https://www.instagram.com/pricelesstori/' target='blank'><FaInstagram /></a>
+            <a href='https://www.twitter.com/ToritsemugboneA/' target='blank'><FaTwitter /></a>
+            <a href='https://www.linkedin.com/in/torrie01/' target='blank'><FaLinkedin /></a>
+            <a href='https://www.youtube.com/torrie/' target='blank'><FaYoutube /></a>
+            <a href='https://portfolio-1-m4oojzuki-gbonetoritse-gmailcom.vercel.app/' target='blank'><FaLink /></a>
         </IconContext.Provider>
         </div>
         <div className='terms'>
